cc07703509ae4b618995668d59e0624904c9a75f
Zach Cheatham <zachcheatham@gmail.com>
2020-10-14 23:23:11 -0500

    Add pip requirements

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    requires = fh.read().splitlines()
+
 setuptools.setup(
     name="aiotuyalan",
-    version="0.0.2",
+    version="0.0.3",
     author="Zach Cheatham",
     author_email="zachcheatham@gmail.com",
     description="Python library for interacting with Tuya-based devices using asyncio",
@@ -18,5 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=requires,
     python_requires='>=3.5.3'
 )
